94f6bac6e38802bc7a0918994c8de91d35f8c091
pedrozampier <pedrootaviopz@gmail.com>
2025-04-30 17:42:52 -0300

    feat: add InviteCard and PartyMemberCard components with styling and functionality

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,9 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Card from "@/components/Card";
 import Title from "@/components/Title";
-import MainCard from "@/components/cards/MainCard";
+import MainCard from "@/components/cards/PartyMemberCard";
+import InviteCard from "@/components/cards/InviteCard";
+import PartyMemberCard from "@/components/cards/PartyMemberCard";
 
 export default function index() {
   return (
@@ -22,9 +24,22 @@ export default function index() {
 
       <Title>MEMBERS</Title>
 
-      <Card>
-        <Text>index</Text>
-      </Card>
+      <InviteCard>
+        <Text>Invite a member</Text>
+      </InviteCard>
+
+      <PartyMemberCard
+        name="Sophiala"
+        username="@bestieee"
+        imgUrl="https://example.com/avatar.jpg"
+        level={12}
+        role="Leader"
+        hpPercentage={0.8}
+        mpPercentage={0.6}
+      />
+
+
+
 
       <Card>
         <Text>index</Text>
